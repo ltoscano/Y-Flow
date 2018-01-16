a8309d7da7faefbcef59e2dcdc73b0be27928ece
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-16 15:02:23 -0500

    first

---
+++ run.sh
@@ -0,0 +1,2 @@
+python matchzoo/main.py --phase train --model_file examples/toy_example/config/arci_ranking.config
+python matchzoo/main.py --phase predict --model_file examples/toy_example/config/arci_ranking.config
