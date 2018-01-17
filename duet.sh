761b2c945871a4bdb81ce96319c7eacaeb60d8df
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-17 13:31:11 -0500

    ne

---
+++ duet.sh
@@ -0,0 +1,2 @@
+python matchzoo/main.py --phase train --model_file examples/sw/config/duet_ranking.config
+python matchzoo/main.py --phase predict --model_file examples/sw/config/duet_ranking.config
