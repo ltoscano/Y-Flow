22a65cb741d5f79e91582aa0ac4c4029d16a2406
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-17 14:06:33 -0500

    ddY

--- examples/toy_example/generate_ranking_data.sh
+++ examples/toy_example/generate_ranking_data.sh
@@ -4,9 +4,9 @@
 python test_preparation_for_ranking.py
 
 # 1. download embedding 
-wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
-mv glove.6B.50d.txt ../../data/toy_example/ranking/
+#wget http://nlp.stanford.edu/data/glove.6B.zip
+#unzip glove.6B.zip
+#mv glove.6B.50d.txt ../../data/toy_example/ranking/
 # 2. map word embedding
 python gen_w2v.py ../../data/toy_example/ranking/glove.6B.50d.txt ../../data/toy_example/ranking/word_dict.txt ../../data/toy_example/ranking/embed_glove_d50
 python norm_embed.py  ../../data/toy_example/ranking/embed_glove_d50 ../../data/toy_example/ranking/embed_glove_d50_norm
