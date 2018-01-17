0e063e48718bfa3ddf1578e6a16ad94eb351ff38
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-17 13:37:37 -0500

    sh file

--- duet.sh
+++ duet.sh
@@ -1,2 +1,2 @@
-python matchzoo/main.py --phase train --model_file examples/sw/config/duet_ranking.config
-python matchzoo/main.py --phase predict --model_file examples/sw/config/duet_ranking.config
+python matchzoo/main.py --phase train --model_file examples/sw/config/duet_ranking_en.config
+python matchzoo/main.py --phase predict --model_file examples/sw/config/duet_ranking_en.config
