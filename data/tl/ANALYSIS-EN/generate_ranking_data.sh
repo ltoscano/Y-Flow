f6e5fb6fd7e1b49c2770bca02adc02f2878d212e
erfan <erfan.ghadery@gmail.com>
2018-01-18 20:04:27 +0330

    setting up tl language scripts and directories..

---
+++ data/tl/ANALYSIS-EN/generate_ranking_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# generate matchzoo data for ranking
+python sample.py
+python test_preparation_for_ranking.py
+
+# 1. download embedding 
+#wget http://nlp.stanford.edu/data/glove.6B.zip
+#unzip glove.6B.zip
+#mv glove.6B.50d.txt ../../data/toy_example/ranking/
+
+#wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.vec
+#mv wiki.en.vec ../../fastext/
+
+# 2. map word embedding
+python gen_w2v.py ../../fastext/wiki.en.vec word_dict.txt embed_fastext_d300
+python norm_embed.py embed_fastext_d300 embed_fastext_d300_norm
+
+# 3. run to generate histogram for DRMM
+python test_histogram_generator.py  'ranking'
+
+# 4. run to generate tri-grams for DSSM or CDSSM
+#python test_triletter_preprocess.py 'ranking'
+
+# 5. run to generate binsum for aNMM
+#python test_binsum_generator.py 'ranking'
+
---
+++ data/tl/ANALYSIS-EN/sample.py
@@ -0,0 +1,50 @@
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+import re
+import os
+import nltk
+nltk.download('punkt')
+tokenizer = RegexpTokenizer(r'\w+')
+
+w = open('sample.txt','w') 
+q2d  = {}
+docs = {}
+q2text = {}
+with open('../topics/en.t') as f:
+    for line in f.readlines():
+        tokens = tokenizer.tokenize(line)
+        #print(a)
+        #tokens = line.rstrip().split(' ')
+        #Text = ''
+        #for x in  tokens[1:len(tokens)-1]:
+        #    Text = Text.join(' '+x)
+        Text = ' '.join(tokens[1:len(tokens)-1])
+        print(tokens[0],Text)
+        q2text[tokens[0]] =  Text #''.join(word_tokenize(f)) for f in tokens[1:len(tokens)-1]
+f.close()
+
+with open('../judg/rel.judg') as f:
+    for line in f.readlines():
+        tokens = line.rstrip().split(' ')
+        if tokens[0] in q2d:
+            q2d[tokens[0]].append(tokens[2])
+        else:
+            q2d[tokens[0]] = [tokens[2]]
+f.close()
+for filename in os.listdir('docs/'):
+    if filename.endswith(".txt"): 
+        with open(os.path.join('docs/', filename)) as f:
+            data = f.read().replace('\r\n',' ')
+            m = re.search('MATERIAL_BASE(.+?)\.', filename)
+            if m:
+                filename = 'MATERIAL_BASE'+m.group(1)
+            docs[filename] = str(data)#''.join(str(w) for w in data)
+            #print(docs[filename])
+        f.close()
+for q in q2d:
+    for d in docs:
+        if d in q2d[q]:
+            w.write('1\t'+q2text[q]+'\t'+docs[d]+'\n')
+        else:
+            w.write('0\t'+q2text[q]+'\t'+docs[d]+'\n')
+w.close()
