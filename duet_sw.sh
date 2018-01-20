554f1059006f7ceb59528027d83a75bcde012927
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-19 21:06:27 -0500

    remove duet_sw

--- duet_sw.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python matchzoo/main.py --phase train --model_file examples/sw/config/duet_ranking_en.config
-python matchzoo/main.py --phase predict --model_file examples/sw/config/duet_ranking_en.config
