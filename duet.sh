54c74a5f9a19845c7f6c8279a93334404274b1c4
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-21 13:15:32 -0500

    dd

--- duet.sh
+++ duet.sh
@@ -1,2 +1,2 @@
-python matchzoo/main.py --phase train --model_file examples/tl/config/duet_ranking_en.config
-python matchzoo/main.py --phase predict --model_file examples/tl/config/duet_ranking_en.config
+python matchzoo/main.py --phase train --model_file examples/tl/config/duet_ranking_tl.config # _en,_tl,_sw
+python matchzoo/main.py --phase predict --model_file examples/tl/config/duet_ranking_tl.config # _en, _tl,_sw
---
+++ duet.sw.sh
@@ -0,0 +1,2 @@
+python matchzoo/main.py --phase train --model_file examples/sw/config/duet_ranking_en.config # _en,_tl,_sw
+python matchzoo/main.py --phase predict --model_file examples/sw/config/duet_ranking_en.config # _en, _tl,_sw
