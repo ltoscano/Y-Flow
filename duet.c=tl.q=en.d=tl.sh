c3436fcfa7ab0cb4ff2c07572967843ff6bc52e9
javiddadashkarimi <javiddadashkarimi@gmail.com>
2018-01-22 00:36:15 -0500

    virtual env

--- duet.c=tl.q=en.d=tl.sh
+++ duet.c=tl.q=en.d=tl.sh
@@ -2,8 +2,8 @@
 for  i in 0.0 0.2 0.4 0.6 0.8
 do
 	python 5-fold.py --phase train --split $i --model_file examples/tl/config/duet_ranking_tl.config # _en,_tl,_sw
-	python matchzoo/main.py --phase train --model_file examples/tl/config/duet_ranking_tl.config # _en,_tl,_sw
-	python matchzoo/main.py --phase predict --model_file examples/tl/config/duet_ranking_tl.config # _en, _tl,_sw
+	python yflow/main.py --phase train --model_file examples/tl/config/duet_ranking_tl.config # _en,_tl,_sw
+	python yflow/main.py --phase predict --model_file examples/tl/config/duet_ranking_tl.config # _en, _tl,_sw
 	mv "predict.test.duet_ranking.txt" "predict.$i.txt"
 done
 
---
+++ yflow-env/lib/python2.7/genericpath.py
@@ -0,0 +1,1 @@
+/home/lily/jd2392/anaconda2/lib/python2.7/genericpath.py
