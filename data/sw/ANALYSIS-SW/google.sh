fc4d3efa76fc78cd883abeb3a9293c55c792b6fd
IreneZihuiLi <zihui.li@ucdconnect.ie>
2018-01-22 20:00:39 -0500

    added google.out

--- data/sw/ANALYSIS-SW/google.sh
+++ data/sw/ANALYSIS-SW/google.sh
@@ -1,9 +1,9 @@
-rm tools/SCORING/*
-rm tools/REFERENCE/*
-rm tools/VALIDATION/*
-IndriRunQuery qmodel/google.sw -index=index/sw -count=100 -trecFormat=true > result/result.first
+#rm tools/SCORING/*
+#rm tools/REFERENCE/*
+#rm tools/VALIDATION/*
+IndriRunQuery qmodel/sw_google -index=index/ -count=20 -trecFormat=true > result/result.first
 python param/trim_result.py #result.file
-trec_eval -q judg/rel.judg result/result.file > google.out
-python indri_to_nist_result.py
-cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'  > tools/SCORING/all.AQWVscores.tsv
-cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'| awk '{T+= $NF} END { print T/NR }'
+trec_eval -q ../judg/rel.judg result/result.file > google.out
+#python indri_to_nist_result.py
+#cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'  > tools/SCORING/all.AQWVscores.tsv
+#cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'| awk '{T+= $NF} END { print T/NR }'
--- data/sw/ANALYSIS-SW/wiktionary.sh
+++ data/sw/ANALYSIS-SW/wiktionary.sh
@@ -1,10 +1,11 @@
 #rm tools/SCORING/*
 #rm tools/REFERENCE/*
 #rm tools/VALIDATION/*
-python src/wiktionary.py --src_lang en --tgt_lang sw --query ../topics/en.t --tquery qmodel/wiktionary --dico_train ../../data/dictionary/en-sw.txt --rank 5
-IndriRunQuery qmodel/wiktionary -index=index/ -count=20 -trecFormat=true > result/result.first
+#python src/wiktionary.py --dico_train sw.csv --tquery qmodel/sw_google --query sw.csv --rank 5
+python src/wiktionary.py --src_lang en --tgt_lang sw --query ../topics/en.t --tquery qmodel/fastext --dico_train ../../dictionary/en-sw.txt --rank 5
+IndriRunQuery qmodel/fastext -index=index/ -count=20 -trecFormat=true > result/result.first
 python param/trim_result.py #result.file
-mv result/result.file result/result.wiktionary
+mv result/result.file result/result.fastext
 trec_eval -q ../judg/rel.judg result/result.wiktionary > wiktionary.out
 #python indri_to_nist_result.py
 #cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'  > tools/SCORING/all.AQWVscores.tsv
