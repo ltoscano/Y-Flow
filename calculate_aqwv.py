475a56385e5a0922fe12a03615a1c146ece7813d
Rui Zhang <r.zhang@yale.edu>
2018-02-16 14:55:03 -0500

    add aqwv

--- calculate_aqwv.py
+++ calculate_aqwv.py
@@ -6,10 +6,27 @@ def main(argv):
   parser.add_argument('--predict', default='', help='')
   parser.add_argument('--judge', default='', help='')
   parser.add_argument('--total', type=int, help='')
+  parser.add_argument('--language', default='sw', help='')
   parser.add_argument('--beta', type=float, default=20.0, help='')
 
   args = parser.parse_args()
   N_total = args.total
+
+  query_type = {}
+  query_topics = {}
+  if args.language == 'sw':
+    topic_file = open('data/sw/topics/en.t')
+  else:
+    topic_file = open('data/tl/topics/en.t')
+  for line in topic_file.readlines():
+    split_line = line.strip().split()
+    query = split_line[0]
+    if ',' in line:
+      query_type[query] = 'conjunction'
+    elif '"' in line:
+      query_type[query] = 'phrase'
+    else:
+      query_type[query] = 'single'
 
   judge_file = open(args.judge)
   judge = {}
@@ -21,6 +38,8 @@ def main(argv):
       judge[query] = []
     judge[query].append(doc)
 
+  print >> sys.stderr, len(judge)
+
   predict_file = open(args.predict)
   predict = {}
   for line in predict_file.readlines():
@@ -31,8 +50,8 @@ def main(argv):
       predict[query] = []
     predict[query].append(doc)
 
-  
-  #assert set(predict.keys()) == set(judge.keys())
+  print >> sys.stderr, len(predict)
+
   cutoffs = [1,3,5,10,20,-1]
   def get_optimal_aqwv(query,q_pred,q_judge):
     N_rel = len(q_judge)
@@ -53,7 +72,10 @@ def main(argv):
       #  print N_correct,N_FA,N_miss
       #  print
 
-      aqwv = 1 - (N_miss / float(N_rel) + args.beta*(N_FA / float(N_nonrel)))
+      if N_rel > 0:
+        aqwv = 1 - (N_miss / float(N_rel) + args.beta*(N_FA / float(N_nonrel)))
+      else:
+        aqwv = 1 - args.beta*(N_FA / float(N_nonrel))
       if optimal_aqwv is None:
         optimal_aqwv = aqwv
         optimal_cutoff = cutoff
@@ -63,13 +85,46 @@ def main(argv):
     return optimal_aqwv, optimal_cutoff
 
   optimal = {}
-  for query in predict.keys():
-    optimal_aqwv,optimal_cutoff = get_optimal_aqwv(query,predict[query],judge[query])
+  for query in query_type.keys():
+    if query not in predict:
+      q_pred = []
+    else:
+      q_pred = predict[query]
+
+    if query not in judge:
+      q_judge = []
+    else:
+      q_judge = judge[query]
+
+    optimal_aqwv,optimal_cutoff = get_optimal_aqwv(query,q_pred,q_judge)
     optimal[query] = (optimal_aqwv,optimal_cutoff)
     print query, "%.4f" % optimal_aqwv, optimal_cutoff
 
-  avg_optimal = sum(optimal[q][0] for q in optimal) / len(judge)
-  print "Average Optimal: %.4f" % avg_optimal
+  avg_optimal = sum(optimal[q][0] for q in optimal) / len(optimal)
+  avg_cutoff = sum(optimal[q][1] for q in optimal) / len(optimal)
+  print >> sys.stderr, len(optimal)
+  print >> sys.stderr, "Average Optimal: %.4f, %.2f" % (avg_optimal,avg_cutoff)
+
+  optimal_single = [optimal[q][0] for q in optimal if query_type[q] is 'single']
+  cutoff_single = [optimal[q][1] for q in optimal if query_type[q] is 'single']
+  avg_optimal_single = sum(optimal_single) / len(optimal_single)
+  avg_cutoff_single = sum(cutoff_single) / len(cutoff_single)
+  print >> sys.stderr, len(optimal_single)
+  print >> sys.stderr, "Average Optimal Single Word: %.4f, %.2f" % (avg_optimal_single,avg_cutoff_single)
+
+  optimal_phrase = [optimal[q][0] for q in optimal if query_type[q] is 'phrase']
+  cutoff_phrase = [optimal[q][1] for q in optimal if query_type[q] is 'phrase']
+  avg_optimal_phrase = sum(optimal_phrase) / len(optimal_phrase)
+  avg_cutoff_phrase = sum(cutoff_phrase) / len(cutoff_phrase)
+  print >> sys.stderr, len(optimal_phrase)
+  print >> sys.stderr, "Average Optimal Phrase: %.4f, %.2f" % (avg_optimal_phrase,avg_cutoff_phrase)
+
+  optimal_conjunction = [optimal[q][0] for q in optimal if query_type[q] is 'conjunction']
+  cutoff_conjunction = [optimal[q][1] for q in optimal if query_type[q] is 'conjunction']
+  avg_optimal_conjunction = sum(optimal_conjunction) / len(optimal_conjunction)
+  avg_cutoff_conjunction = sum(cutoff_conjunction) / len(cutoff_conjunction)
+  print >> sys.stderr, len(optimal_conjunction)
+  print >> sys.stderr, "Average Optimal Conjunction: %.4f, %.2f" % (avg_optimal_conjunction, avg_cutoff_conjunction)
 
 if __name__=='__main__':
   main(sys.argv)
