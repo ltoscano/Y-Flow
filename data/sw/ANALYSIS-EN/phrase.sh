8a771ea361b46f0c41811441c47d3222e32a6429
dadashkarimi <javiddadashkarimi@gmail.com>
2018-04-17 04:24:50 -0400

    -q option

--- data/sw/ANALYSIS-EN/phrase.sh
+++ data/sw/ANALYSIS-EN/phrase.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
 CUTOFF=$1
+QUERY='../topics/QUERY1/query_list.tsv'
+if [ -n "$2" ]; then
+  QUERY=$2
+fi
 
-python src/phrase.py --phrase True --src_lang en --tgt_lang en --query ../topics/QUERY1/query_list.tsv --tquery qmodel/mono --model mono
+python src/phrase.py --phrase True --src_lang en --tgt_lang en --query $QUERY  --tquery qmodel/mono --model mono
 IndriRunQuery qmodel/mono -index=index/ -count=$CUTOFF -trecFormat=true > result/result.first
 python param/trim_local_result.py # generate result.file
 rm result/result.first
 trec_eval -q -N 471 ../judg/rel.analysis result/result.file > phrase.out.cutoff$CUTOFF
-#python indri_to_nist_result.py
-#cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'  > tools/SCORING/all.AQWVscores.tsv
-#cut -f 8 tools/SCORING/* | grep -vE "(Beta|AQWV)" | awk 'NF'| awk '{T+= $NF} END { print T/NR }'
--- data/tl/ANALYSIS-EN/phrase.sh
+++ data/tl/ANALYSIS-EN/phrase.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
 CUTOFF=$1
+QUERY='../topics/QUERY1/query_list.tsv'
+if [ -n "$2" ]; then
+  QUERY=$2
+fi
 
-python src/phrase.py --phrase True --src_lang en --tgt_lang en --query ../topics/QUERY1/query_list.tsv --tquery qmodel/mono --model mono
+
+python src/phrase.py --phrase True --src_lang en --tgt_lang en --query $QUERY --tquery qmodel/mono --model mono
 IndriRunQuery qmodel/mono -index=index/ -count=$CUTOFF -trecFormat=true > result/result.first
 python param/trim_local_result.py # generate result.file
 rm result/result.first
